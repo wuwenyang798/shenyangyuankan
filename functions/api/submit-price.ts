9fd27cf36f102674d59917dd52d85a167687d410
wuwenyang798 <18278274745@163.com>
2026-06-15 10:42:03 +0800

    fix: 修复询价通知模块路径

--- functions/api/submit-price.ts
+++ functions/api/submit-price.ts
@@ -1,5 +1,5 @@
 import { priceFormSchema, clean } from '../../src/utils/validateForm';
-import { notifyAdminNewPriceRequest } from '../_shared/notify-admin';
+import { notifyAdminNewPriceRequest } from './_shared/notify-admin';
 
 type RuntimeEnv = {
   DB?: D1Database;
